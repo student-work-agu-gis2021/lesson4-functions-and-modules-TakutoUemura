b957bbfba2ac77626a70eb86fc52de6bc846e92f
TakutoUemura <>
2021-05-14 03:04:40 +0000

    I added a script file called temp_functions.py.

--- temp_functions.py
+++ temp_functions.py
@@ -4,7 +4,7 @@ def fahr_to_celsius(temp_fahrenheit):
   converted_temp = (temp_fahrenheit - 32) / 1.8
   return converted_temp
 
-  """Substituting Celsius for temp_celsius divides the temperature into one of four classes and returns the value in that class."""
+"""Substituting Celsius for temp_celsius divides the temperature into one of four classes and returns the value in that class."""
 def temp_classifier(temp_celsius):
   if temp_celsius < -2:
      return 0
